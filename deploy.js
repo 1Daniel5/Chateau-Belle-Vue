e8b3014a4ef50975d5ac69ed7f00807a2d251197
patrickharuna66 <your.email@example.com>
2026-06-19 15:16:15 -0700

    Changes to be committed:

--- deploy.js
+++ deploy.js
@@ -8,7 +8,7 @@ const commands = [
   'git add .',
   'git commit -m "deploy"',
   'git branch -M gh-pages',
-  'git remote add origin https://github.com/1Daniel5/chateau-belle-vue.git',
+  'git remote add origin https://github.com/1Daniel5/Chateau-Belle-Vue.git',
   'git push -f origin gh-pages',
   'cd ..'
 ];
@@ -16,7 +16,7 @@ const commands = [
 try {
   execSync(commands.join(' && '), { stdio: 'inherit', shell: true });
   console.log('\n Deployed successfully!');
-  console.log(' Visit: https://1Daniel5.github.io/chateau-belle-vue');
+  console.log(' Visit: https://1Daniel5.github.io/Chateau-Belle-Vue');
 } catch (error) {
   console.error('\n Deploy failed:', error.message);
   process.exit(1);
